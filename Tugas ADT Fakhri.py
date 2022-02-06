423ac2d70a9f6a2bc5003809a1af7453e8bb79ff
FakhriAlwan50 <99125674+FakhriAlwan50@users.noreply.github.com>
2022-02-06 19:47:44 +0700

    Update Tugas ADT Fakhri.py

--- Tugas ADT Fakhri.py
+++ Tugas ADT Fakhri.py
@@ -1,3 +1,9 @@
+#assalamualaikum warahmatullahi wabarakatuh, nama saya fakhri alwan dengan npm G1A021050. berikut adalah penerapan adt yang berfungsi untuk menghitung luas persegi panjang pada bahasa python
+#menggunakan fungsi "input" untuk mengisi sebuah panjang dan lebar suatu persegi panjang
+#int digunakan untuk menerima dua masukan parameter, berikut adalah source codenya:
+
 panjang = input("masukkan panjang : ")
 lebar = input("masukkan lebar : ")
 print("luas persegi panjang adalah : " , int(panjang)* int(lebar))
+
+#sekian penjelasan singkat dari saya mohon maaf jika ada kesalahan. terimakasih
